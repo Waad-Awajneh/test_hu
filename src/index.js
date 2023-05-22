1a6af0d28a16ed7041bc1b7ffaa4d2ec5b953125
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-05-22 10:03:28 +0300

    deleting strict mode

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-// react
 import React from "react";
-// react-router-dom
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-
-// store, react-redux, redux-persist
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
-
-// components
 import App from "./App";
-
-// styles
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
@@ -26,7 +18,7 @@ root.render(
         </PersistGate>
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+
 );
 
 reportWebVitals();
