17e469c02794661a7a129964b9e4b638ffd1c37a
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2023-05-22 10:14:42 +0300

    adding path /

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { store, persistor } from "./redux/store";
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // ]);
 root.render(
   <React.StrictMode>
-    <BrowserRouter path={"/"}>
+    <BrowserRouter path="/">
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
